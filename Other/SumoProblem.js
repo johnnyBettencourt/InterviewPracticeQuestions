1b61a7b2da8a914af6b075363fc8a23c648fd8a3
johnnyBettencourt <johnnybettencourt75@gmail.com>
2023-10-10 10:07:55 -0700

    added comments

--- Other/SumoProblem.js
+++ Other/SumoProblem.js
@@ -1,42 +1,56 @@
-const testArr = [1, 3, 5, 8, 2, 3, 7]
+// Define an array of power levels for sumo wrestlers
+const testArr = [1, 3, 5, 8, 2, 3, 7];
 
+// Define a function to determine the winner in a sumo competition
 function sumoWinner(arr) {
-    const newArr = arr
+    // Create a copy of the input array
+    const newArr = arr;
+
+    // Continue looping until there is only one sumo wrestler left in the array
     while (newArr.length > 1) {
+        // Initialize objects to keep track of the best and second-best sumo wrestlers
         let bestSumo = {
-                power: 0,
-                index: 0,
-            };
-            let secondBestSumo = {
-                power: 0,
-                index: 0,
-            };
-        for (let i = 0; i <= newArr.length; i++){
-            
+            power: 0,
+            index: 0,
+        };
+        let secondBestSumo = {
+            power: 0,
+            index: 0,
+        };
+
+        // Iterate through the array to find the best and second-best sumo wrestlers
+        for (let i = 0; i < newArr.length; i++) {
             if (newArr[i] > bestSumo.power) {
+                // If the current sumo wrestler has more power than the best sumo so far, update bestSumo
                 bestSumo.power = newArr[i];
                 bestSumo.index = i;
-            }
-            else{
+            } else {
                 if (newArr[i] > secondBestSumo.power) {
+                    // If the current sumo wrestler has more power than the second-best sumo so far, update secondBestSumo
                     secondBestSumo.power = newArr[i];
                     secondBestSumo.index = i;
                 }
             }
         }
-        if(bestSumo.index > secondBestSumo.index){
+
+        // Determine the indexes of the best and second-best sumo wrestlers and remove them from the array
+        if (bestSumo.index > secondBestSumo.index) {
             newArr.splice(bestSumo.index, 1);
             newArr.splice(secondBestSumo.index, 1);
-        }
-        else{
+        } else {
             newArr.splice(secondBestSumo.index, 1);
             newArr.splice(bestSumo.index, 1);
         }
+
+        // Calculate the difference in power between the best and second-best sumo wrestlers and push it into the array
         newArr.push(bestSumo.power - secondBestSumo.power);
-        // console.log(`${bestSumo.power} VS. ${secondBestSumo.power}`);
-        // console.log(newArr);
+
+        // Continue the loop until there is only one sumo wrestler left in the array
     }
-    return newArr
+
+    // Return the final array with the winner's power level
+    return newArr;
 }
 
+// Call the sumoWinner function with the testArr and log the result to the console
 console.log(sumoWinner(testArr));
